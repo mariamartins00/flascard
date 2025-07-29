4a35607d15790b172872629f35774727a2fe2138
mariamartins00 <maria.martins.chagas@escola.pr.gov.br>
2025-07-29 16:25:14 -0300

    29/07

---
+++ assets/perguntas.js
@@ -0,0 +1,19 @@
+criacartao(
+    'stranger things',
+    'Quem é a personagem com poderes psíquicos que escapa de um laboratório secreto?',
+    'Eleven (Onze)'
+)
+
+criacartao(
+    'stranger things',
+    'Qual o nome do mundo alternativo sombrio na série?',
+    'O Mundo Invertido Upside Down' 
+   )
+
+criacartao(
+    'stranger things',
+    'Como se chama o grupo de amigos protagonistas da série?',
+    'Mike, Dustin, Lucas e Will (mais tarde Max e Eleven se juntam'
+)
+
+
